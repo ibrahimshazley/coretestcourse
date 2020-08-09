dea04a36d76c0ee3847d34e0671e564ae45f50c7
ibrahim shazley <shazley.cs@gmail.com>
2020-08-09 06:34:03 +0200

    initial commit

---
+++ CoreTest/CoreTest/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using CoreTest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) :base (options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
